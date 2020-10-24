18eedb8c8b7c6ab85b97c168ce8106c2da4ba976
DiscordTheOwO <54290097+DiscordTheOwO@users.noreply.github.com>
2020-10-24 15:17:06 -0500

    Main GUI
    Did Main screen

---
+++ TechnicaHacks/Assets/changeTxtBtnOne.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class changeTxtBtnOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //[SerializeField] TextMeshProGUI m_Object;
+   // private TextMeshProUGUI button;
+    public TextMeshProUGUI txt;
+    void Start()
+    {
+        //  GetComponent<changeTxtBtnOne>().Text = "Scan using AR.";
+        // GameObject.Find("changeTxtBtnOne").GetComponentInChildren<Text>().text = "Scan using AR.";
+        //changeTxtBtnOne button = GetComponentInChildren<changeTxtBtnOne>();
+        // changeTxtBtnOne button = GetComponentInChildren<TextMeshProUGUI>();
+        // button.Text = "Scan using AR";
+        //  m_Object = GetComponent<TextMeshProUGUI>().text = "Scan using AR.";
+
+        //button = GetComponent<TextMeshProUGUI>();
+
+       txt.text = "Scan using AR.";
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
