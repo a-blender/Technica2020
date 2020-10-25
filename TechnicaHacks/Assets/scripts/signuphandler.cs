9cc8f279870cb7071e67ee94be9c136f8e9bce42
stanburya <67989967+stanburya@users.noreply.github.com>
2020-10-24 23:26:27 -0400

    assistant with voice

--- TechnicaHacks/Assets/scripts/signuphandler.cs
+++ TechnicaHacks/Assets/scripts/signuphandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Firebase;
-using Firebase.Database;
 using Firebase.Unity.Editor;
 using Firebase.Auth;
 using UnityEngine.SceneManagement;
